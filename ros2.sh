d8aa6bcef7bc391ddf916d151e754e7946c7e421
Luke Chang <luke.x.chang@gmail.com>
2023-11-08 02:02:02 +1300

    fix typo

--- ros2.sh
+++ ros2.sh
@@ -1,5 +1,5 @@
 export TURTLEBOT3_MODEL=waffle
-export RMW_IMPLEMENTATION=rwm_cyclonedds_cpp
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 source /opt/ros/humble/setup.bash
 source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
 source ~/ws_ros2/install/setup.bash
